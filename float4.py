283c5f60d4d0230ae64944e11689527a6b30d2cc
Nagesh576 <rudraramnagesh29@gmail.com>
2025-12-15 09:45:25 +0000

    exp4

---
+++ float4.py
@@ -0,0 +1,5 @@
+p = float(input("Enter principal amount: "))
+t = float(input("Enter time (years): "))
+r = float(input("Enter rate of interest: "))
+si = (p * t * r) / 100
+print("Simple Interest:", si)
