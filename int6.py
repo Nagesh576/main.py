075908b8c95c84606f5388c141dbba033e7706b4
Nagesh Rudraram <rudraramnagesh29@gmail.com>
2026-08-13 01:30:42 +0000

    Second commit

--- int6.py
+++ int6.py
@@ -1,3 +1,7 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Sum =", a + b)
+
+c = int(input("Enter third number: "))
+d = int(input("Enter Fourth Number "))
+print("Sum =", c + d)
